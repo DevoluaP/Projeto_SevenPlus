8c124014cce88742e2d1dcb31a573888959facf3
Paulo Henrique <paulohenrique456moreira@gmail.com>
2024-06-23 15:15:29 -0300

    '#'

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// document.getElementById('btnEsqueciSenha').addEventListener('click', function () {
+//     if (document.getElementById('email').value == null) {
+//         document.getElementById('formEsqueciSenha').setAttribute('action', '#');
+//     } 
+// });
+
+// document.getElementById('btnNovaSenha').addEventListener('click', function () {
+//     if (document.getElementById('senha').value !== document.getElementById('confSenha').value || document.getElementById('senha').value == null || document.getElementById('confSenha').value == null) {
+//         document.getElementById('formNovaSenha').setAttribute('action', '#');
+//     } 
+// });
